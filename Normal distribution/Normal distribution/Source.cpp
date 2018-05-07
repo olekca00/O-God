a1b00925a475d9b224ca06429cdda4d3aadc4552
olekca00 <olekca00@gmail.com>
2018-05-07 12:05:53 +0300

    )

--- Normal distribution/Normal distribution/Source.cpp
+++ Normal distribution/Normal distribution/Source.cpp
@@ -1,3 +1,35 @@
+/*
+#include <iostream>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+#include<boost/math/distributions/normal.hpp>
+using namespace boost::math;
+
+int main(void)
+{
+	double mean_life = 80.;
+	double life_standard_deviation = 10.;
+	normal bulbs(mean_life, life_standard_deviation);
+	double expected_life = 100.;
+
+	std::cout << "Fraction of bulbs that will last at best [<=] " // P(X <= 60)
+		<< expected_life << " is " << cdf(bulbs, expected_life) << std::endl;
+	std::cout << "Fraction of bulbs that will last at least [>] " // P(X > 60)
+		<< expected_life << " is " << cdf(complement(bulbs, expected_life)) << std::endl;
+
+	double min_life = 70;
+	double max_life = 100;
+	std::cout << "Fraction of bulbs that will last between "
+		<< min_life << " and " << max_life << " is "
+		<< cdf(bulbs, max_life) // P(X <= 100)
+		- cdf(bulbs, min_life) << std::endl; // P(X <= 70)
+
+	system("pause");
+}
+*/
+
+
 #include <iostream>
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
@@ -11,7 +43,7 @@ int main(void)
 	// distribution that the user wants
 	boost::variate_generator<boost::mt19937, boost::normal_distribution<> >
 		gen(igen,
-			boost::normal_distribution<>(1, 0.1));
+			boost::normal_distribution<>(0,1));
 
 	const size_t n = 10;
 	for (size_t i = 0; i<n; ++i)
